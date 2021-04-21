1f138b4a779c4d5f433e21404c8646bdc5a921ef
Andre Bun <andreebun97@gmail.com>
2021-04-21 09:31:45 +0700

    Load data bank di payment evidence page

--- payment-evidence.php
+++ payment-evidence.php
@@ -21,7 +21,7 @@
 ?>
 <div class="image-login-register d-flex justify-content-end">
     <img src="assets/photo/kosan.jpg" class="card-img-top" alt="...">
-    <div class="container login-container d-flex justify-content-center align-items-center position-fixed" style="top: 50%;left: 50%;transform: translate(-50%, -50%); background:white; width: 500px;padding-top: 30px;padding-bottom: 30px;overflow-y: auto;">
+    <div class="container login-container d-flex justify-content-center align-items-center position-fixed" style="top: 50%;left: 50%;transform: translate(-50%, -50%); background:white; width: 500px;padding-top: 70px;padding-bottom: 30px;overflow-y: auto;">
         <form class="form-horizontal" enctype="multipart/form-data">
             <h3 style="text-align: center;">Payment Evidence</h3>
             <div class="form-group">
@@ -36,16 +36,33 @@
                     <input type="text" readonly class="form-control" name="total_payment" />
                 </div>
             </div>
+            <?php
+                $bank_data = array();
+                include "DB_connection.php";
+                $database = new Database();
+                $con = $database->getConnection();
+
+                $bank_sql = "SELECT bn.* from banks AS bn";
+
+                $banks = $con->query($bank_sql);
+
+                // echo $room_sql;
+                // print_r($rooms['num_rows']);
+                if($banks->num_rows > 0){
+                    while($row = $banks->fetch_assoc()) {
+                        array_push($bank_data, $row);
+                    }
+                }
+                $con->close();
+            ?>   
             <div class="form-group">
                 <label class="control-label">Transfer to</label>
+                <?php for($k = 0; $k < count($bank_data); $k++):?>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="paymentmethod1" id="flexRadioDefault1">
-                    <label class="form-check-label" for="flexRadioDefault1">1222898409 - Bambang Subambang (BCA)</label>
+                    <label class="form-check-label" for="flexRadioDefault1"><?= $bank_data[$k]['bank_account_number'].' - '.$bank_data[$k]['owner_name'].' ('.$bank_data[$k]['bank_name'].')' ;?></label>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="paymentmethod2" id="flexRadioDefault2">
-                    <label class="form-check-label" for="flexRadioDefault2">1238910209123 - John Cena (Mandiri)</label>
-                </div>
+                <?php endfor;?>
             </div>
             <div class="form-group">
                 <label class="control-label">Payment Date</label>
