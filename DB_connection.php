86297fccb0a1fcf45f145a6fcb480655e00999ae
Andre Bun <andreebun97@gmail.com>
2021-04-24 11:04:24 +0700

    Konfigurasi database

--- DB_connection.php
+++ DB_connection.php
@@ -1,11 +1,18 @@
 <?php
 
+    include "DB_configuration.php";
+
     class Database{
         public $con;
+        public $db_config;
         
+        public function __construct(){
+            $db_config = new DatabaseConfiguration();
+            $this->db_config = $db_config;
+        }
         public function getConnection(){
             $con = null;
-            $con = mysqli_connect('localhost','root','','mykosan');
+            $con = mysqli_connect($this->db_config->getServerName(),$this->db_config->getUsername(),$this->db_config->getPassword(),$this->db_config->getDatabaseName());
                     
             if ($con->connect_error) {
                 die("Connection failed: " . $con->connect_error);
