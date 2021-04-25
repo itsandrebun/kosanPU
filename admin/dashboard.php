74f47f376eb964f377d1fec3d135959210299ab0
Andre Bun <andreebun97@gmail.com>
2021-04-25 18:58:44 +0700

    Merubah tampilan pada dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -31,6 +31,8 @@
         <?php
           
           include "templates/navbar.php";
+          include "../Helpers/Currency.php";
+          $currency = new Currency();
         ?>
 
         <!-- Begin Page Content -->
@@ -39,9 +41,83 @@
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary mykosan-signature-button-color shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
           </div>
 
+          <?php
+            // $tenant_data = array();
+
+            // $tenant_sql = "SELECT us.* FROM user AS us JOIN tenant AS tn ON tn.user_id = us.user_id";
+
+            // $tenants = $con->query($tenant_sql);
+
+            //     // echo $room_sql;
+            //     // print_r($rooms['num_rows']);
+            // if($tenants->num_rows > 0){
+            //     while($row = $tenants->fetch_assoc()) {
+            //         array_push($tenant_data, $row);
+            //     }
+            // }
+            // $con->close();
+            $chosen_month = date('m');
+            if(isset($_GET['month'])){
+              $chosen_month = $_GET['month'];
+            }
+            $chosen_year = date('Y');
+            if(isset($_GET['year'])){
+              $chosen_year = $_GET['year'];
+            }
+            $booking_statistics_sql = "SELECT (SELECT (CASE WHEN COUNT(tr.transaction_id) IS NULL THEN 0 ELSE COUNT(tr.transaction_id) END) FROM transaction AS tr WHERE CURRENT_TIMESTAMP < tr.booking_start_date) AS total_pending_booking, (SELECT (CASE WHEN COUNT(tr.transaction_id) IS NULL THEN 0 ELSE COUNT(tr.transaction_id) END) FROM transaction AS tr WHERE CURRENT_TIMESTAMP BETWEEN tr.booking_start_date AND tr.booking_end_date) AS total_active_booking, (SELECT (CASE WHEN COUNT(tr.transaction_id) IS NULL THEN 0 ELSE COUNT(tr.transaction_id) END) FROM transaction AS tr WHERE CURRENT_TIMESTAMP > tr.booking_end_date) AS total_expired_booking, (SELECT (CASE WHEN COUNT(tr.transaction_id) IS NULL THEN 0 ELSE COUNT(tr.transaction_id) END) FROM transaction AS tr WHERE terminated_date IS NOT NULL) AS total_terminated_booking";
+
+            $payment_statistics_sql = "SELECT (SELECT (CASE WHEN SUM(inv.total_payment) IS NULL THEN 0 ELSE SUM(inv.total_payment) END) FROM invoice AS inv WHERE inv.payment_status <= 2) AS total_pending_payment, (SELECT (CASE WHEN SUM(inv.total_payment) IS NULL THEN 0 ELSE SUM(inv.total_payment) END) FROM invoice AS inv WHERE inv.payment_status = 3) AS total_approved_payment, (SELECT (CASE WHEN SUM(inv.total_payment) IS NULL THEN 0 ELSE SUM(inv.total_payment) END) FROM invoice AS inv WHERE inv.payment_status = 4) AS total_rejected_payment";
+
+            $booking_statistics = $con->query($booking_statistics_sql);
+            $booking_statistics = $booking_statistics->fetch_assoc();
+            $payment_statistics = $con->query($payment_statistics_sql);
+            $payment_statistics = $payment_statistics->fetch_assoc();
+          ?>
+          <form method="GET" class="mb-4 p-3" style="border:0.5px solid #c5c3c3;border-radius:1%;">
+            <h5>Filter</h5>
+            <div class="row mb-2">
+                <div class="col-md-4">
+                    <select name="month" id="" class="form-control">
+                      <option value="all">Choose Month</option>
+                      <option value="01" <?= !empty($chosen_month) && $chosen_month == '01' ? 'selected' : '' ;?>>January</option>
+                      <option value="02" <?= !empty($chosen_month) && $chosen_month == '02' ? 'selected' : '' ;?>>February</option>
+                      <option value="03" <?= !empty($chosen_month) && $chosen_month == '03' ? 'selected' : '' ;?>>March</option>
+                      <option value="04" <?= !empty($chosen_month) && $chosen_month == '04' ? 'selected' : '' ;?>>April</option>
+                      <option value="05" <?= !empty($chosen_month) && $chosen_month == '05' ? 'selected' : '' ;?>>May</option>
+                      <option value="06" <?= !empty($chosen_month) && $chosen_month == '06' ? 'selected' : '' ;?>>June</option>
+                      <option value="07" <?= !empty($chosen_month) && $chosen_month == '07' ? 'selected' : '' ;?>>July</option>
+                      <option value="08" <?= !empty($chosen_month) && $chosen_month == '08' ? 'selected' : '' ;?>>August</option>
+                      <option value="09" <?= !empty($chosen_month) && $chosen_month == '09' ? 'selected' : '' ;?>>September</option>
+                      <option value="10" <?= !empty($chosen_month) && $chosen_month == '10' ? 'selected' : '' ;?>>October</option>
+                      <option value="11" <?= !empty($chosen_month) && $chosen_month == '11' ? 'selected' : '' ;?>>November</option>
+                      <option value="12" <?= !empty($chosen_month) && $chosen_month == '12' ? 'selected' : '' ;?>>December</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <?php
+                        $year = date('Y');
+                    ?>
+                    <select name="year" id="" class="form-control">
+                      <!-- <option value="all">Choose Year</option> -->
+                      <?php for($k = ($year - 4); $k < ($year + 5); $k++):?>
+                          <option value="<?= $k ;?>" <?= !empty($chosen_year) && $chosen_year == $k ? 'selected' : '' ;?>><?= $k ;?></option>
+                      <?php endfor;?>
+                    </select>
+                </div>
+            </div>
+            <div class="d-flex justify-content-end">
+              <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                  <div class="btn-group me-2" role="group" aria-label="First group">
+                      <button type="button" class="btn btn-danger" onclick="window.location.href = 'dashboard'">Reset</button>
+                  </div>
+                  <div class="btn-group ml-2" role="group" aria-label="Second group">
+                      <button type="submit" class="btn btn-primary mykosan-signature-button-color">Search</button>
+                  </div>
+              </div>
+            </div>
+          </form>
           <!-- Content Row -->
           <div class="row">
 
@@ -51,11 +127,11 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Booking (Pending)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $booking_statistics['total_pending_booking'];?></div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                      <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
@@ -68,11 +144,11 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Booking (Active)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $booking_statistics['total_active_booking'];?></div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                      <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
@@ -81,41 +157,31 @@
 
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
+              <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                        </div>
-                        <div class="col">
-                          <div class="progress progress-sm mr-2">
-                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                          </div>
-                        </div>
-                      </div>
+                      <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Booking (Expired)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $booking_statistics['total_expired_booking'];?></div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                      <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-warning shadow h-100 py-2">
+              <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                      <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Booking (Terminated)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $booking_statistics['total_terminated_booking'];?></div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+                      <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
@@ -124,76 +190,60 @@
           </div>
 
           <!-- Content Row -->
-
           <div class="row">
 
-            <!-- Area Chart -->
-            <div class="col-xl-8 col-lg-7">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                  <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                      <div class="dropdown-header">Dropdown Header:</div>
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                  </div>
-                </div>
-                <!-- Card Body -->
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-4 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
-                  <div class="chart-area">
-                    <canvas id="myAreaChart"></canvas>
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Payment (Pending)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 'Rp '.$currency->convert($payment_statistics['total_pending_payment']);?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-money-bill fa-2x text-gray-300"></i>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <!-- Pie Chart -->
-            <div class="col-xl-4 col-lg-5">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                  <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                      <div class="dropdown-header">Dropdown Header:</div>
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-4 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Payment (Accepted)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 'Rp '.$currency->convert($payment_statistics['total_approved_payment']);?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-money-bill fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
-                <!-- Card Body -->
+              </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-4 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
-                  <div class="chart-pie pt-4 pb-2">
-                    <canvas id="myPieChart"></canvas>
-                  </div>
-                  <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-primary"></i> Direct
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Social
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-info"></i> Referral
-                    </span>
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Payment (Rejected)</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= 'Rp '.$currency->convert($payment_statistics['total_rejected_payment']);?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-money-bill fa-2x text-gray-300"></i>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <!-- Content Row -->
 
           <!-- Content Row -->
           <div class="row">
@@ -204,7 +254,7 @@
               <!-- Project Card Example -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Most Booked Rooms</h6>
                 </div>
                 <div class="card-body">
                   <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
@@ -289,7 +339,7 @@
               <!-- Illustrations -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Most Active Users</h6>
                 </div>
                 <div class="card-body">
                   <div class="text-center">
