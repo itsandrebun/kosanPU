fa987b864c391707b56b9d1090d54dbd8bb0a4b1
Andre Bun <andreebun97@gmail.com>
2021-04-25 19:00:33 +0700

    Merubah tampilan pada dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -339,7 +339,7 @@
               <!-- Illustrations -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Most Active Users</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Most Active Tenants</h6>
                 </div>
                 <div class="card-body">
                   <div class="text-center">
